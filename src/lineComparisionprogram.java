f33f13c4005737fab257c78ddffc748f8b70f5e2
shubhangi1208 <ashubhangi1996@gmail.com>
2021-12-23 17:40:23 +0530

    Added lenght of line

--- src/lineComparisionprogram.java
+++ src/lineComparisionprogram.java
@@ -1,6 +1,16 @@
+import java.lang.Math.*;
 
 public class lineComparisionprogram {
-public static void main(String[] args) {
-System.out.println("Welcome to Line Comparision Computation Program");
+
+	public static void main(String[] args) {
+       System.out.println("Welcome to Line Comparision Computation Program");
+       int x1,x2,y1,y2;
+       double length;
+       x1=1;
+       x2=3;
+       y1=2;
+       y2=4;
+     length=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+     System.out.println("Length is "+length );
 }
 }
