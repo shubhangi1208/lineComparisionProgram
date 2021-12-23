64885419e45a3bb2c9b65c08e49dd7abe4bf39da
shubhangi1208 <ashubhangi1996@gmail.com>
2021-12-23 19:15:03 +0530

    Added: equality of two lines

--- src/lineComparisionprogram.java
+++ src/lineComparisionprogram.java
@@ -4,13 +4,27 @@ public class lineComparisionprogram {
 
 	public static void main(String[] args) {
        System.out.println("Welcome to Line Comparision Computation Program");
-       int x1,x2,y1,y2;
-       double length;
+       int x1,x2,x3,x4,y1,y2,y3,y4;
+       double Distance1,Distance2;
        x1=1;
        x2=3;
        y1=2;
        y2=4;
-     length=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
-     System.out.println("Length is "+length );
-}
+     Distance1=Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+     System.out.println("Distance1 is "+ Distance1 );
+     
+     
+     x3=2;
+     x4=3;
+     y3=2;
+     y4=4;
+   Distance2=Math.sqrt((x4-x3)*(x4-x3) + (y4-y3)*(y4-y3));
+   System.out.println("Distance2 is "+ Distance2 );
+     if(Distance1==Distance2)
+    		 {
+    		 System.out.println("Two lines are equal");
+    		 }
+     else
+    	 System.out.println("Two lines are not equal");
+	}
 }
